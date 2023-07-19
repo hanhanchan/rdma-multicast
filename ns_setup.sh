dc90f5df437feb69dbca9a7f5307e99c50bb74ea
hanhanchan <40458024+hanhanchan@users.noreply.github.com>
2023-07-19 08:41:01 +0800

    git commit .

--- ns_setup.sh
+++ ns_setup.sh
@@ -27,7 +27,7 @@ sudo ip netns exec ns2 arp -s 10.10.10.4 42:1a:cb:44:54:a6
 sudo ip netns add ns3
 sudo ip link set ens2f0 netns ns3
 sudo ip netns exec ns3 ip link set lo up
-sudo ip netns exec ns3 ip link set ens2f3 up
+sudo ip netns exec ns3 ip link set ens2f2 up
 sudo ip netns exec ns3 ip addr add 10.10.10.3/24 dev ens2f2
 sudo ip netns exec ns3 ip a
 sudo ip netns exec ns3 arp -s 10.10.10.1 f6:45:ec:6e:93:14
@@ -40,7 +40,7 @@ sudo ip netns exec ns3 arp -s 10.10.10.4 42:1a:cb:44:54:a6
 sudo ip netns add ns4
 sudo ip link set ens2f1 netns ns4
 sudo ip netns exec ns4 ip link set lo up
-sudo ip netns exec ns4 ip link set ens2f4 up
+sudo ip netns exec ns4 ip link set ens2f3 up
 sudo ip netns exec ns4 ip addr add 10.10.10.4/24 dev ens2f3
 sudo ip netns exec ns4 ip a
 sudo ip netns exec ns4 arp -s 10.10.10.1 f6:45:ec:6e:93:14
